169d76bc3c616ff214ad2c7e383fe9c16f5db33f
V.V.Revanth <118685357+vvRevanth@users.noreply.github.com>
2024-03-27 22:31:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,78 @@
+
 import streamlit as st
+import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+from dotenv import load_dotenv
+import json
+
+# Load environment variables from the `.env` file
+load_dotenv()
+
+# Configure Streamlit page settings
+st.set_page_config(
+    page_title="Smart ATS",
+    page_icon="‍",  # Use a placeholder for now
+    layout="centered",
+)
+
+# Configure Gemini AI model with the API key from environment variable
+api_key = os.getenv("AIzaSyD2oLQHkz9sYQvKZN6VaZ7ZI2t2N79wefQ")
+genai.configure(api_key=api_key)
+
+# Function to get response from Gemini AI
+def get_gemini_response(input):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input)
+    return response.text
+
+# Function to extract text from uploaded PDF file
+def input_pdf_text(uploaded_file):
+    reader = pdf.PdfReader(uploaded_file)
+    text = ""
+    for page in range(len(reader.pages)):
+        page = reader.pages[page]
+        text += str(page.extract_text())
+    return text
+
+# Prompt Template
+input_prompt = """
+Hey Act Like a skilled or very experienced ATS (Application Tracking System)
+with a deep understanding of the tech field, software engineering, data science, data analyst
+and big data engineering. Your task is to evaluate the resume based on the given job description and skills.
+You must consider the job market is very competitive and you should provide the best assistance for improving the resumes.
+Assign the percentage Matching based on JD and the missing skills/keywords with high accuracy.
+resume:{text}
+description:{jd}
+skills:{skills}
+
+I want the response in one single string having the structure
+{{"JD Match":"%","MissingSkills/Keywords":[],"Profile Summary":""}}
+"""
+
+## Streamlit app
+st.title("Resume Matcher ATS")
+
+jd = st.text_area("Paste the Job Description")
+skills = st.multiselect("Select Skills", ["Python", "Java", "JavaScript", "SQL", "C++", "Machine Learning", "Data Analysis"])
+uploaded_file = st.file_uploader("Upload Your Resume", type="pdf", help="Please upload the PDF")
+
+submit = st.button("Submit")
+
+if submit:
+    if uploaded_file is not None:
+        text = input_pdf_text(uploaded_file)
+        response = get_gemini_response(input_prompt.format(text=text, jd=jd, skills=", ".join(skills)))
+        st.subheader("Response:")
+        parsed_response = json.loads(response)
+        for key, value in parsed_response.items():
+            st.write(f"**{key}:** {value}")
+
+
+
+
+
+'''import streamlit as st
 import google.generativeai as genai
 import os
 import PyPDF2 as pdf
@@ -79,5 +153,5 @@ if submit:
         parsed_response = json.loads(response)
         for key, value in parsed_response.items():
             st.write(f"**{key}:** {value}")
-
+'''
 
