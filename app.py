d24799047a2798997a7b68e49dc184e0e9b8b4eb
V.V.Revanth <118685357+vvRevanth@users.noreply.github.com>
2024-03-27 22:53:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 
-
 import streamlit as st
 import google.generativeai as genai
 import PyPDF2 as pdf
@@ -27,45 +26,6 @@ def input_pdf_text(uploaded_file):
         page = reader.pages[page]
         text += str(page.extract_text())
     return text
-
-description_source = st.radio("Select Job Description Source:", ("From CSV File", "Enter Manually"))
-selected_job_title = None
-jd = None
-
-description_source == "Enter Manually"
-jd = st.text_area("Enter Job Description:")
-
-# Show job description for the first selected job title
-#selected_job_title = selected_job_titles[0]
-#jd = job_postings[job_postings['title'] == selected_job_title]['description'].values[0]
-
-uploaded_file = st.file_uploader("Upload Your Resume", type="pdf", help="Please upload the PDF")
-submit = st.button("Submit")
-
-if submit:
-    if uploaded_file is not None:
-        text = input_pdf_text(uploaded_file)
-        input_prompt = f"""
-        Hey Act Like a skilled or very experienced ATS (Application Tracking System)
-        with a deep understanding of the tech field, software engineering, data science, data analyst
-        and big data engineering. Your task is to evaluate the resume based on the given job description.
-        You must consider the job market is very competitive and you should provide the 
-        best assistance for improving the resumes. Assign the percentage Matching based 
-        on JD and the missing keywords with high accuracy.
-        resume:{text}
-        description:{jd}
-        
-        I want the response in one single string having the structure
-        {{"JD Match":"%","MissingKeywords":[],"Profile Summary":""}}
-        """
-        configure_gemini_api(API_KEY)
-        response = get_gemini_response(input_prompt)
-        st.subheader("Response:")
-        parsed_response = json.loads(response)
-        for key, value in parsed_response.items():
-            st.write(f"**{key}:** {value}")
-
-
 
 '''import streamlit as st
 import google.generativeai as genai
